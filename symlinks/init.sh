da84783ded13c232aebc3d6b31970b53b1f8b7ae
Jack Weinbender <jack.weinbender@gmail.com>
2016-12-18 19:11:19 -0600

    Added zshrc

--- symlinks/init.sh
+++ symlinks/init.sh
@@ -4,6 +4,10 @@
 rm ~/.gitconfig
 ln -s ~/.dotfile/symlinks/.gitconfig ~/.gitconfig
 
+# .zshrc
+rm ~/.zshrc
+ln -s ~/.dotfile/symlinks/.zshrc ~/.zshrc
+
 # .zimrc
 rm ~/.zimrc
 ln -s ~/.dotfile/symlinks/.zimrc ~/.zimrc
