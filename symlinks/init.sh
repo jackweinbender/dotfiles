2ced401125bf96fba37f3cdfa2850f4ae6968412
Jack Weinbender <jack.weinbender@gmail.com>
2017-01-03 20:49:31 -0600

    fixed naming problems

--- symlinks/init.sh
+++ symlinks/init.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # .gitconfig
-ln -s ~/.dotfile/symlinks/.gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/symlinks/.gitconfig ~/.gitconfig
 
 # .zshrc
-ln -s ~/.dotfile/symlinks/.zshrc ~/.zshrc
+ln ~/.dotfiles/symlinks/.zshrc ~/.zshrc
 
 # jack.zsh-theme
-ln -s ~/.dotfile/symlinks/jack.zsh-theme ~/.oh-my-zsh/themes/jack.zsh-theme
+ln -s ~/.dotfiles/symlinks/jack.zsh-theme ~/.oh-my-zsh/themes/jack.zsh-theme
