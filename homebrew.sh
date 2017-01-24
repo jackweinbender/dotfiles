776ca91dad6d774bf9ee5085b6da414468a6d975
Jack Weinbender <jack.weinbender@gmail.com>
2017-01-24 12:34:11 -0600

    Updated brew cask

--- homebrew.sh
+++ homebrew.sh
@@ -21,12 +21,14 @@ brew install zsh
 # Cask
 brew cask install google-chrome
 brew cask install atom
+brew cask install keepingyouawake
 brew cask install dashlane
 brew cask install dropbox
 brew cask install google-drive
 brew cask install skype
 brew cask install transmission
 brew cask install mactex
+brew cask install google-cloud-sdk
 
 # Remove outdated versions from the cellar.
 brew cleanup
