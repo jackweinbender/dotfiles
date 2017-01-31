0d33effe882384d20edd80af5c9d5bcd60003ebe
Jack Weinbender <jack.weinbender@gmail.com>
2017-01-31 10:53:50 -0600

    Rm atom

--- clonables.sh
+++ clonables.sh
@@ -3,6 +3,3 @@
 # Pandoc etc.
 git clone git@github.com:jackweinbender/dot-pandoc.git ~/.pandoc
 git clone git@github.com:jackweinbender/dot-csl.git ~/.csl
-
-# Applications
-git clone git@github.com:jackweinbender/dot-atom.git ~/.atom
