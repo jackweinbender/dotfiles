52d45de0220255407a46b17aadb786fc18e87348
Jack Weinbender <jack.weinbender@gmail.com>
2017-07-06 09:43:17 -0500

    Good starter packages for pacman

--- .arch/pacman.sh
+++ .arch/pacman.sh
@@ -3,3 +3,27 @@
 # Install Zsh and git
 sudo pacman -S git
 sudo pacman -S zsh
+
+# GUI Utils
+sudo pacman -S chromium
+sudo pacman -S dropbox
+sudo pacman -S lastpass
+
+# Development
+sudo pacman -S visual-studio-code
+sudo pacman -S nodejs
+sudo pacman -S npm
+sudo pacman -S rustup
+
+sudo npm install -g yarn
+sudo npm install -g ember-cli
+
+# TeX Stuff
+sudo pacman -S atom-editor-bin
+sudo pacman -S pandoc pandoc-citeproc
+sudo pacman -S texlive-core texlive-localmanager-git
+sudo pacman -S biber
+
+
+# Other
+sudo pacman -S google-cloud-sdk
