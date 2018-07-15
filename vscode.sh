920fdef9f71b269dd0c86b2e884fefb04d1ee8b0
Jack Weinbender <jack.weinbender@gmail.com>
2018-07-15 11:06:52 -0500

    Added vscode installers

---
+++ vscode.sh
@@ -0,0 +1,5 @@
+code --install-extension rust-lang.rust
+code --install-extension emberjs.emberjs
+code --install-extension sdras.night-owl
+code --install-extension 2gua.rainbow-brackets
+code --install-extension eg2.tslint
