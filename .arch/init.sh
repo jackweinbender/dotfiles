bb6cc75b4de024830e67582501c4999de70890ba
Jack Weinbender <jack.weinbender@gmail.com>
2018-07-15 09:25:34 -0500

    Keygen and non-standard inputs

--- .arch/init.sh
+++ .arch/init.sh
@@ -6,7 +6,11 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `setup.hs` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+# Setup SSH Keys
 ssh-keygen -t rsa -b 4096 -C "jack.weinbender@gmail.com"
+
+# Enable non-standard inputs
+gsettings set org.gnome.desktop.input-sources show-all-sources true
 
 source pacman.sh
 source dev.sh
