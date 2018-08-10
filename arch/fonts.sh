15a7a90606734b0d798b503e1f6b8d1dd68beaa6
Jack Weinbender <jack.weinbender@gmail.com>
2018-08-10 15:45:36 -0500

    Added fonts

--- arch/fonts.sh
+++ arch/fonts.sh
@@ -7,3 +7,4 @@ pacaur -S --noconfirm --noedit ttf-adobe-fonts
 pacaur -S --noconfirm --noedit ttf-sbl-greek
 pacaur -S --noconfirm --noedit ttf-sbl-hebrew
 pacaur -S --noconfirm --noedit ttf-ancient-fonts
+pacaur -S --noconfirm --noedit ttf-sil-fonts
