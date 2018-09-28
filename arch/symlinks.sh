2f72fda77ef36c1bb3a11fedba93ccd086583d43
Jack Weinbender <jack.weinbender@gmail.com>
2018-09-28 16:10:03 -0500

    Better symlinks?

--- arch/symlinks.sh
+++ arch/symlinks.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-rm -rf ~/.gitconfig ~/.zshrc ~/.oh-my-zsh/themes/jack.zsh-theme
+rm -rf $HOME/.gitconfig $HOME/.zshrc $HOME/.oh-my-zsh/themes/jack.zsh-theme
 
 # .gitconfig
-ln -s ~/.dotfiles/symlinks/.gitconfig ~/.gitconfig
+ln -s $HOME/.dotfiles/symlinks/.gitconfig $HOME/.gitconfig
 
 # .zshrc
-ln ~/.dotfiles/symlinks/.zshrc ~/.zshrc
+ln $HOME/.dotfiles/symlinks/.zshrc $HOME/.zshrc
 
 # jack.zsh-theme
-ln -s ~/.dotfiles/symlinks/jack.zsh-theme ~/.oh-my-zsh/themes/jack.zsh-theme
+ln -s $HOME/.dotfiles/symlinks/jack.zsh-theme $HOME/.oh-my-zsh/themes/jack.zsh-theme
