b6d188666615d31dad6d9f4805ec68ca553b39a2
Jack Weinbender <jack.weinbender@gmail.com>
2018-09-28 20:06:04 -0500

    Added .conkyrc

--- arch/symlinks.sh
+++ arch/symlinks.sh
@@ -6,7 +6,10 @@ rm -rf $HOME/.gitconfig $HOME/.zshrc $HOME/.oh-my-zsh/themes/jack.zsh-theme
 ln -s $HOME/.dotfiles/symlinks/.gitconfig $HOME/.gitconfig
 
 # .zshrc
-ln $HOME/.dotfiles/symlinks/.zshrc $HOME/.zshrc
+ln -s $HOME/.dotfiles/symlinks/.zshrc $HOME/.zshrc
 
 # jack.zsh-theme
 ln -s $HOME/.dotfiles/symlinks/jack.zsh-theme $HOME/.oh-my-zsh/themes/jack.zsh-theme
+
+# Conky dotfile
+ln -s $HOME/.dotfiles/arch/.conkyrc $HOME/.conkyrc
