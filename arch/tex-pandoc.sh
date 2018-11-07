2135d3f932b72e1d2086d8167dcc0308d539065f
Jack Weinbender <jack.weinbender@gmail.com>
2018-11-07 13:05:01 -0600

    Added tex packages

--- arch/tex-pandoc.sh
+++ arch/tex-pandoc.sh
@@ -2,7 +2,10 @@
 sudo pacman -S --noconfirm pandoc pandoc-citeproc
 
 # TeX Stuff
-sudo pacman -S --noconfirm texlive-core
+sudo pacman -S --noconfirm \
+    texlive-core \
+    texlive-latexextra \
+    texlive-bibtexextra
 sudo pacman -S --noconfirm biber
 pacaur -S --noconfirm texlive-localmanager-git
 
