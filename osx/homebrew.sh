17e104a3c4cb566fefa8684383d26aac284e0be0
Jack Weinbender <jack.weinbender@gmail.com>
2022-02-25 10:52:37 -0600

    Update homebrew.sh

--- osx/homebrew.sh
+++ osx/homebrew.sh
@@ -1,32 +1,25 @@
 #!/usr/bin/env bash
 
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Homebrew Config
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
+source $HOME/.zprofile
+
+# Install Git
+brew install git
+
 # Update Homebrew
 brew update
 
 # Upgrade All
 brew upgrade
 
-# Terminal Deps
-brew install zsh
-brew install make
-
-# Fonts
-brew tap caskroom/fonts
-brew cask install font-hack
-brew cask install font-linux-libertine
-
 # Cask
-brew cask install google-chrome
+brew cask install brave-browser
 brew cask install firefox
-brew cask install dashlane
-brew cask install lastpass
-brew cask install dropbox
-brew cask install google-drive-file-stream
-brew cask install keepingyouawake
-brew cask install flux
 brew cask install visual-studio-code
-brew cask install etcher
-brew cask install transmission
 brew cask install spotify
 
 # Remove outdated versions from the cellar.
