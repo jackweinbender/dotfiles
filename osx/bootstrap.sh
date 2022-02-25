4dabf623188f25d66bfe1035674b597b031bd910
Jack Weinbender <jack.weinbender@gmail.com>
2022-02-25 12:58:39 -0600

    Update bootstrap.sh

--- osx/bootstrap.sh
+++ osx/bootstrap.sh
@@ -6,27 +6,10 @@ function install_devtools(){
     read -r -p "Once installation is complete, press any key to continue..." key
 }
 
-function clone_dotfiles(){
-    git clone https://github.com/jackweinbender/dotfiles.git $HOME/.dotfiles
-}
-
-function setup_zsh(){
-    source $HOME/.dotfiles/zsh/bootstrap.sh
-}
-
-function brew_init(){
-    source $HOME/.dotfiles/osx/brew-init.sh
-}
-
-function install_brew_recipes(){
-    source $HOME/.dotfiles/osx/brew.sh
-}
-
 # Make sure Git is installed first
 command -v git || install_devtools()
 
-# Strapit
-clone_dotfiles()
-setup_zsh()
-brew_init()
-install_brew_recipes()
+git clone https://github.com/jackweinbender/dotfiles.git $HOME/.dotfiles
+source $HOME/.dotfiles/zsh/bootstrap.sh
+source $HOME/.dotfiles/osx/brew-init.sh
+source $HOME/.dotfiles/osx/brew.sh
