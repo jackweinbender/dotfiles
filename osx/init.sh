284cb8509c90013b290fe16c5f14685aaed2d40b
Jack Weinbender <jack.weinbender@gmail.com>
2022-02-25 11:11:00 -0600

    Update init.sh

--- osx/init.sh
+++ osx/init.sh
@@ -1,25 +1,4 @@
 #!/usr/bin/env bash
 
-# Install Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-# Homebrew Config
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
-source $HOME/.zprofile
-
-# Core (Don't use Apple's version)
-brew install git
-
-# Development
-brew install docker
-brew install nvm
-brew install rbenv
-brew install rustup
-brew install python3
-brew install watchman
-
-# Cask
-brew cask install brave-browser
-brew cask install firefox
-brew cask install visual-studio-code
-brew cask install spotify
+source $DOTFILES/osx/homebrew/init.sh
+source $DOTFILES/osx/homebrew/requirements.sh
