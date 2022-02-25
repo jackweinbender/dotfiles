9a9b79f2affa5875e1a87e27bf7fbaeefa5c6d06
Jack Weinbender <jack.weinbender@gmail.com>
2022-02-25 11:00:01 -0600

    Update homebrew.sh

--- osx/homebrew.sh
+++ osx/homebrew.sh
@@ -7,20 +7,19 @@
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
 source $HOME/.zprofile
 
-# Install Git
+# Core (Don't use Apple's version)
 brew install git
 
-# Update Homebrew
-brew update
-
-# Upgrade All
-brew upgrade
+# Development
+brew install docker
+brew install nvm
+brew install rbenv
+brew install rustup
+brew install python3
+brew install watchman
 
 # Cask
 brew cask install brave-browser
 brew cask install firefox
 brew cask install visual-studio-code
 brew cask install spotify
-
-# Remove outdated versions from the cellar.
-brew cleanup
