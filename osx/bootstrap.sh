321d102e33785b65af4d877b870c571ca3a82f56
Jack Weinbender <jack.weinbender@gmail.com>
2022-02-25 10:29:24 -0600

    Update bootstrap.sh

--- osx/bootstrap.sh
+++ osx/bootstrap.sh
@@ -1,12 +1,7 @@
 #! /bin/bash
 
-# Download this file, make it executable, and run it.
-
-# Install Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-# Install Git
-brew install git
+# Install Devtools
+xcode-select --install
 
 # Clone dotfiles
 git clone https://github.com/jackweinbender/dotfiles.git ~/.dotfiles
