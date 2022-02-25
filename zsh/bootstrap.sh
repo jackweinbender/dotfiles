e7422df8728bb69eca9fed1bf59b96287d28a725
Jack Weinbender <jack.weinbender@gmail.com>
2022-02-25 13:22:32 -0600

    Update bootstrap.sh

--- zsh/bootstrap.sh
+++ zsh/bootstrap.sh
@@ -30,7 +30,7 @@ DOTFILES="$(dirname "${0:a:h}")"
 # Export the base DOTFILES variable
 cat <<EOM >>~/.zshrc
 # Path for the "dotfiles" (defaults to ~/dotfiles)
-export DOTFILES=${DOTFILES}
+export DOTFILES=${HOME}/.dotfiles
 
 EOM
 
