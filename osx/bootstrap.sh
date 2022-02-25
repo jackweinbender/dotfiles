acf8ed3792674f7fb822a05639de655d237c8ba6
Jack Weinbender <jack.weinbender@gmail.com>
2022-02-25 11:27:36 -0600

    Update bootstrap.sh

--- osx/bootstrap.sh
+++ osx/bootstrap.sh
@@ -12,5 +12,5 @@ git clone https://github.com/jackweinbender/dotfiles.git $HOME/.dotfiles
 
 # Source MacOS Initialization:
 source $HOME/.dotfiles/zsh/bootstrap.sh
-source $HOME/.dotfiles/osx/homebrew/init.sh
-source $HOME/.dotfiles/osx/homebrew/install.sh
+source $HOME/.dotfiles/osx/brew-init.sh
+source $HOME/.dotfiles/osx/brew.sh
