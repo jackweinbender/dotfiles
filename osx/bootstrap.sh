339cfae55c852bff43fb03d557083d0f30b6e23c
Jack Weinbender <jack.weinbender@gmail.com>
2022-02-25 10:09:58 -0600

    Update bootstrap.sh

--- osx/bootstrap.sh
+++ osx/bootstrap.sh
@@ -10,8 +10,3 @@ brew install git
 
 # Clone dotfiles
 git clone https://github.com/jackweinbender/dotfiles.git ~/.dotfiles
-
-read -p  "You may wish to edit the setup file.\npress any key to continue, or CTRL + C to abort."
-
-# Run the installer
-./~/.dotfiles/osx/osx-setup.sh
