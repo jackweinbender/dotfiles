0ddc2d7bf09d3d52dd146034678d69a8caea0dbb
Jack Weinbender <jack.weinbender@gmail.com>
2022-02-25 11:10:03 -0600

    Create requirements.sh

---
+++ osx/homebrew/requirements.sh
@@ -0,0 +1,16 @@
+# Core (Don't use Apple's version)
+brew install git
+
+# Development
+brew install docker
+brew install nvm
+brew install rbenv
+brew install rustup
+brew install python3
+brew install watchman
+
+# Cask
+brew cask install brave-browser
+brew cask install firefox
+brew cask install visual-studio-code
+brew cask install spotify
