eaf00823b6cc387ac499bd442e1ff8bf9c0e78e5
Jack Weinbender <jack.weinbender@gmail.com>
2022-02-25 11:08:49 -0600

    Update init.sh

--- osx/homebrew/init.sh
+++ osx/homebrew/init.sh
@@ -1,1 +1,8 @@
+#!/usr/bin/env bash
 
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Homebrew Config
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
+source $HOME/.zprofile
