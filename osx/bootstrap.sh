0cc764a85bd7c4a0dfbee6713793e1855869cbb7
Jack Weinbender <jack.weinbender@gmail.com>
2022-02-25 12:44:16 -0600

    Update bootstrap.sh

--- osx/bootstrap.sh
+++ osx/bootstrap.sh
@@ -1,27 +1,45 @@
 #! /bin/bash
 
-# Check for GIT
-if ! command -v git &> /dev/null
-then
+function install_devtools(){
     echo "Installing XCode Dev Tools..."
     xcode-select --install
     read -r -p "Once installation is complete, press any key to continue..." key
-fi
+}
 
-read -p "Would you like to check for updates to MacOS before proceeding? [Y/n]" -n 1 -r
-echo ''
-if [[ $REPLY =~ ^[Nn]$ ]]
-then
+function update_osx(){
     softwareupdate -a -l
-fi
-echo ''
+}
 
-# Clone dotfiles
-git clone https://github.com/jackweinbender/dotfiles.git $HOME/.dotfiles
+function prompt_for_update(){
+    read -p "Would you like to check for updates to MacOS before proceeding? [Y/n]"  -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Nn]$ ]]
+    then
+        update_osx()
+    fi
+}
 
-# Terminal / ZSH Setup
-source $HOME/.dotfiles/zsh/bootstrap.sh
+function clone_dotfiles(){
+    git clone https://github.com/jackweinbender/dotfiles.git $HOME/.dotfiles
+}
 
-# Source MacOS Initialization:
-source $HOME/.dotfiles/osx/brew-init.sh
-source $HOME/.dotfiles/osx/brew.sh
+function setup_zsh(){
+    source $HOME/.dotfiles/zsh/bootstrap.sh
+}
+
+function brew_init(){
+    source $HOME/.dotfiles/osx/brew-init.sh
+}
+
+function install_brew_recipes(){
+    source $HOME/.dotfiles/osx/brew.sh
+}
+
+# Main
+command -v git || install_devtools()
+prompt_for_update()
+
+clone_dotfiles()
+setup_zsh()
+brew_init()
+install_brew_recipes()
