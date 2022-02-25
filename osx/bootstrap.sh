4ce8d742a75e7af410443a2b37ae366d4bd87b79
Jack Weinbender <jack.weinbender@gmail.com>
2022-02-25 11:44:44 -0600

    Update bootstrap.sh

--- osx/bootstrap.sh
+++ osx/bootstrap.sh
@@ -3,14 +3,17 @@
 # Check for GIT
 if ! command -v git &> /dev/null
 then
-    echo "${green}Installing XCode Dev Tools${\green}"
+    echo "Installing XCode Dev Tools..."
     xcode-select --install
+    read -r -p "Once installation is complete, press any key to continue..." key
 fi
 
 # Clone dotfiles
 git clone https://github.com/jackweinbender/dotfiles.git $HOME/.dotfiles
 
-# Source MacOS Initialization:
+# Terminal / ZSH Setup
 source $HOME/.dotfiles/zsh/bootstrap.sh
+
+# Source MacOS Initialization:
 source $HOME/.dotfiles/osx/brew-init.sh
 source $HOME/.dotfiles/osx/brew.sh
