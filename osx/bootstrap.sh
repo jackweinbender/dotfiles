13f068c4c3192a4b1d9ac6f20a8c55fda770f8d5
Jack Weinbender <jack.weinbender@gmail.com>
2022-02-25 12:50:53 -0600

    Update bootstrap.sh

--- osx/bootstrap.sh
+++ osx/bootstrap.sh
@@ -6,15 +6,6 @@ function install_devtools(){
     read -r -p "Once installation is complete, press any key to continue..." key
 }
 
-function prompt_for_update(){
-    read -p "Would you like to check for updates to MacOS before proceeding? [Y/n]"  -n 1 -r
-    echo
-    if [[ ! $REPLY =~ ^[Yy]$ ]]
-    then
-        [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
-    fi
-}
-
 function clone_dotfiles(){
     git clone https://github.com/jackweinbender/dotfiles.git $HOME/.dotfiles
 }
