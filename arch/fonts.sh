731dba2e49d53670761b764bda919f5f028299fc
Jack Weinbender <jack.weinbender@gmail.com>
2022-02-25 20:56:51 -0600

    Delete fonts.sh

--- arch/fonts.sh
+++
@@ -1,9 +0,0 @@
-# Fonts
-sudo pacman -S --noconfirm ttf-linux-libertine
-sudo pacman -S --noconfirm ttf-hack 
-#yay -S --noconfirm --noedit ttf-ms-fonts
-#yay -S --noconfirm --noedit ttf-mac-fonts
-#yay -S --noconfirm --noedit ttf-sbl-greek
-#yay -S --noconfirm --noedit ttf-sbl-hebrew
-#yay -S --noconfirm --noedit ttf-ancient-fonts
-#yay -S --noconfirm --noedit ttf-sil-fonts
