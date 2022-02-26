1768c120816f66edb06ba0e0bac5bac82dd16ba3
Jack Weinbender <jack.weinbender@gmail.com>
2022-02-25 20:51:43 -0600

    Update pacman.sh

--- arch/pacman.sh
+++ arch/pacman.sh
@@ -1,20 +1,6 @@
 #!/usr/bin/env bash
 
-# Install Essentials for completion
-#sudo pacman -S --noconfirm git
-#sudo pacman -S --noconfirm zsh
-
-# Screen Sharing Setup
-#sudo pacman -S --noconfirm vino
-  ## Allows Win/Mac ScreenShareing
-  #gsettings set org.gnome.Vino require-encryption false
-
 # GUI Utils
+sudo pacman -S --noconfirm brave
 sudo pacman -S --noconfirm chromium
 sudo pacman -S --noconfirm firefox
-sudo pacman -S --noconfirm conky
-
-# AUR packages
-yay -S --noconfirm dropbox
-#yay -S --noconfirm zoom
-#yay -S --noconfirm slack-desktop
