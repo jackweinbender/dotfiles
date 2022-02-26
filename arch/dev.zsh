771e10ca380d1fb9b4a3e3a1e149eb363fb77f99
Jack Weinbender <jack.weinbender@gmail.com>
2022-02-25 21:02:02 -0600

    Update and rename dev.sh to dev.zsh

--- arch/dev.zsh
+++ arch/dev.zsh
@@ -1,17 +1,7 @@
-# Development
-
 # Code Editors
 sudo pacman -S --noconfirm gconf
 yay -S --noconfirm code
 sudo pacman -S --noconfirm vim
-
-## Virtualbox
-sudo pacman -S --noconfirm linux-headers
-sudo pacman -S --noconfirm vagrant
-sudo pacman -S --noconfirm virtualbox
-  ## These are required for Arch, they replace the DKMS packages
-  sudo pacman -S --noconfirm virtualbox-host-modules-arch
-  sudo pacman -S --noconfirm virtualbox-guest-modules-arch
 
 ## Docker
 sudo pacman -S --noconfirm docker
@@ -21,8 +11,9 @@ sudo pacman -S --noconfirm docker-machine
 sudo usermod -aG docker ${USER}
 sudo systemctl enable docker.service
 
-## Web & Node
+## Node
 sudo pacman -S --noconfirm nodejs
+sudo pacman -S --noconfirm npm
 
 ## Rust
 sudo pacman -S --noconfirm rustup
@@ -30,6 +21,3 @@ sudo pacman -S --noconfirm rustup
 ## Ruby
 yay -S --noconfirm rbenv
 yay -S --noconfirm ruby-build
-
-# Google Cloud SDK
-#yay -S --noconfirm google-cloud-sdk
