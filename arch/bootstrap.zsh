5c3bc17c5a4a9399906de92186367e89bae1c4ac
Jack Weinbender <jack.weinbender@gmail.com>
2022-02-25 20:55:59 -0600

    Update bootstrap.zsh

--- arch/bootstrap.zsh
+++ arch/bootstrap.zsh
@@ -1,1 +1,11 @@
-sudo pacman -S brave
+# Update OS
+sudo pacman -Syyu
+sudo pacman -S base-devel
+
+# Install yay
+## Install yay for AUR
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si --noconfirm
+cd ../
+rm -rf yay
