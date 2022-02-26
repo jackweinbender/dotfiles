51178b93d23b0a45ed293cb33cffdc5dba2c3f2e
Jack Weinbender <jack.weinbender@gmail.com>
2022-02-25 21:15:48 -0600

    Update bootstrap.zsh

--- arch/bootstrap.zsh
+++ arch/bootstrap.zsh
@@ -3,11 +3,3 @@ sudo pacman -Syyu
 
 # Install yay
 pacman -S --needed git base-devel && install_from_git yay
-
-function install_from_git()
-{
-  git clone https://aur.archlinux.org/${1}.git
-  cd $1
-  makepkg -si --noconfirm
-  cd ../ && rm -rf $1
-}
