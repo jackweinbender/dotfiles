6553e31dcf9f802f5b5451b52b600344ebb12bc8
Jack Weinbender <jack.weinbender@gmail.com>
2024-04-09 16:52:28 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,11 @@
 #! /bin/zsh
 
-source zsh/lib/color_helpers.zsh
-source zsh/lib/output_helpers.zsh
+function bootstrap_init() {
+  # Need these helpers to be required so we can get the pretty output on the 
+  # first go-round
+  source zsh/lib/color_helpers.zsh
+  source zsh/lib/output_helpers.zsh
+}
 
+bootstrap_init()
 source zsh/bootstrap.zsh
